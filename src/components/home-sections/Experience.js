f9ecd198c342025b72b5e48fd551693a203c37d1
andrew <andrewras58@gmail.com>
2025-02-23 08:52:35 -0800

    reelresemblance added to experience tab

--- src/components/home-sections/Experience.js
+++ src/components/home-sections/Experience.js
@@ -24,6 +24,23 @@ function Experience() {
           "https://img.shields.io/badge/gunicorn-%298729.svg?style=for-the-badge&logo=gunicorn&logoColor=white",
         ]}
       />
+      <ExperienceWidget
+        image={require("../../assets/reelresemblance.png")}
+        link="https://reelresemblance.com"
+        description="I made a personal project that uses AI to query a MariaDB Database I created using TMDB and OMDB APIs to find similar movies to a given input.
+          It uses FastAPI for the backend with models-weights I pre-trained to run comparisons on the fly, React for the frontend, and it is hosted via Docker containers on Linode.
+          I have plans to: further expand the project by implementing user logins to save viewed movies to curate recommendations better, spruce up the UX, and implement automatic updates of the dataset.
+        "
+        time="October 2024"
+        title="Reel Resemblance"
+        technologies={[
+          "https://img.shields.io/badge/FastAPI-009485.svg?style=for-the-badge&logo=fastapi&logoColor=white",
+          "https://img.shields.io/badge/React-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB",
+          "https://img.shields.io/badge/Hugging%20Face-FFD21E?style=for-the-badge&logo=huggingface&logoColor=000",
+          "https://img.shields.io/badge/Docker-2496ED?style=for-the-badge&logo=docker&logoColor=fff",
+          "https://img.shields.io/badge/linode-00A95C?style=for-the-badge&logo=linode&logoColor=white"
+        ]}
+      />
     </div>
   );
 }
